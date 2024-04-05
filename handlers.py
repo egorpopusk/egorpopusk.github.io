a8d275f6f551b62268898264a4fd3a85e0463c25
egorpopusk <149186442+egorpopusk@users.noreply.github.com>
2024-04-05 21:10:11 +0300

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -1,3 +1,9 @@
+from main import bot, dp
+from keyboards import keyboard
+from aiogram import types
+from aiogram.dispatcher.filters import Command
+
+@dp.message_handler(Command('start'))
 async def start(message: types.Message):
     await bot.send_message(message.chat.id, 'Тестируем WebApp',
                            reply_markup=keyboard)
